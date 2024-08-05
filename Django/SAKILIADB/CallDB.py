4a51ffd00ceb6314fcdcadf60fe6b907e1353507
Kai Siang <peter82615@yahoo.com.tw>
2024-08-06 01:20:15 +0800

    複習至20240730

--- Django/SAKILIADB/CallDB.py
+++ Django/SAKILIADB/CallDB.py
@@ -1,3 +1,8 @@
 from DB_DEMO import Category
 
-print(Category.categories())
+# 讀取
+print(Category.category_all())
+print(Category.category_single(4))
+
+# 新增
+print(Category.category_create('erthjkmngfdsd'))
--- Django/SAKILIADB/DB_DEMO.py
+++ Django/SAKILIADB/DB_DEMO.py
@@ -1,20 +1,31 @@
 import MySQLdb
 
+
 class Category():
     def __init__():
         pass
 
-    def categories():
-        # 第一步 建立連線
-        connection = MySQLdb.connect(
-            host='localhost',
-            database='sakila',
-            user='root',
-            password='Kai114615'
-        )
-        if not connection:
+    def create_connection():
+        # 第一步 建立連線(重複相同段)
+        try:
+            connection = MySQLdb.connect(
+                host='localhost',
+                database='sakila',
+                user='root',
+                password='Kai114615'
+            )
+            return connection
+        except MySQLdb.MySQLError as e:
+            print(f"資料讀取錯誤: {e}")
             return None
 
+    # 讀取所有資料
+
+    def category_all():
+        # 第一步 建立連線
+        connection = Category.create_connection()
+        if not connection:
+            return None
 
         # 第二步 SQL語法
         sql = 'SELECT * FROM category'
@@ -32,5 +43,47 @@ class Category():
                     print(f"資料讀取錯誤: {e}")
                     return None
 
+    # 讀取單一資料
+    def category_single(id):
+        # 第一步 建立連線
+        connection = Category.create_connection()
+        if not connection:
+            return None
 
+        # 第二步 SQL語法
+        sql = f'SELECT * FROM category where category_id=%s'
 
+        # 第三步 建立cursor語法執行SQL語法
+        with connection:
+            with connection.cursor() as cursor:
+                try:
+                    cursor.execute(sql, (id,))
+
+                    # 第三步之一 讀取資料
+                    results = cursor.fetchone()
+                    return results
+                except MySQLdb.MySQLError as e:
+                    print(f"資料讀取錯誤: {e}")
+                    return None
+
+    # 新增資料
+    def category_create(category_name):
+        connection = Category.create_connection()
+        if not connection:
+            return None
+
+        # 第二步 新增SQL資料語法
+        sql = 'insert into category (name) values (%s)'
+
+        # 第三步 建立cursor語法執行SQL語法
+        with connection:
+            with connection.cursor() as cursor:
+                try:
+                    cursor.execute(sql, (category_name,))
+
+                    # 第三步之二 讀取資料
+                    cursor.commit()
+                    return cursor.rowcount
+                except MySQLdb.MySQLError as e:
+                    print(f"資料新增失敗: {e}")
+                    return None
