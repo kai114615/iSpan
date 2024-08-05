2b14d6a4ed230a24635ae0ae47ab8b91bd7e6dc1
Kai Siang <peter82615@yahoo.com.tw>
2024-08-06 01:43:20 +0800

    複習到20240730

--- Django/SAKILIADB/CallDB.py
+++ Django/SAKILIADB/CallDB.py
@@ -2,7 +2,13 @@ from DB_DEMO import Category
 
 # 讀取
 print(Category.category_all())
-print(Category.category_single(4))
+print(Category.category_single(1))
 
 # 新增
-print(Category.category_create('erthjkmngfdsd'))
+# print(Category.category_create('erthjkmngfdsd'))
+
+# 修改
+# print(Category.category_update(18, 'erth'))
+
+# 刪除
+print(Category.category_delete(17))
--- Django/SAKILIADB/DB_DEMO.py
+++ Django/SAKILIADB/DB_DEMO.py
@@ -1,5 +1,7 @@
 import MySQLdb
 
+# 以下用with語法，離開程式區塊cursor、connection物件就會自動被釋放掉，因此不需要第四步驟
+
 
 class Category():
     def __init__():
@@ -20,7 +22,6 @@ class Category():
             return None
 
     # 讀取所有資料
-
     def category_all():
         # 第一步 建立連線
         connection = Category.create_connection()
@@ -86,4 +87,48 @@ class Category():
                     return cursor.rowcount
                 except MySQLdb.MySQLError as e:
                     print(f"資料新增失敗: {e}")
+                    return None
+
+    # 修改資料
+    def category_update(id, category_name):
+        connection = Category.create_connection()
+        if not connection:
+            return None
+
+        # 第二步 修改SQL資料語法
+        sql = 'update category set name=%s where category_id=%s'
+
+        # 第三步 建立cursor語法執行SQL語法
+        with connection:
+            with connection.cursor() as cursor:
+                try:
+                    cursor.execute(sql, (category_name, id))
+
+                    # 第三步之二 讀取資料
+                    connection.commit()
+                    return cursor.rowcount
+                except MySQLdb.MySQLError as e:
+                    print(f"資料修改失敗: {e}")
+                    return None
+
+    # 刪除資料
+    def category_delete(id):
+        connection = Category.create_connection()
+        if not connection:
+            return None
+
+        # 第二步 刪除SQL資料語法
+        sql = 'delete from category where category_id=%s'
+
+        # 第三步 建立cursor語法執行SQL語法
+        with connection:
+            with connection.cursor() as cursor:
+                try:
+                    cursor.execute(sql, (id,))
+
+                    # 第三步之二 讀取資料
+                    connection.commit()
+                    return cursor.rowcount
+                except MySQLdb.MySQLError as e:
+                    print(f"資料刪除失敗: {e}")
                     return None
