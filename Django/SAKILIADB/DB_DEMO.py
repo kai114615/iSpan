118708b1cb6c201455aed0617cc32948725b1870
Kai Siang <peter82615@yahoo.com.tw>
2024-08-05 20:47:37 +0800

    複習到20240730

--- Django/SAKILIADB/DB_DEMO.py
+++ Django/SAKILIADB/DB_DEMO.py
@@ -5,24 +5,32 @@ class Category():
         pass
 
     def categories():
+        # 第一步 建立連線
         connection = MySQLdb.connect(
             host='localhost',
             database='sakila',
             user='root',
-            password='kai114615'
+            password='Kai114615'
         )
         if not connection:
             return None
-        
 
+
+        # 第二步 SQL語法
         sql = 'SELECT * FROM category'
 
+        # 第三步 建立cursor語法執行SQL語法
         with connection:
             with connection.cursor() as cursor:
                 try:
                     cursor.execute(sql)
+
+                    # 第三步之一 讀取資料
                     results = cursor.fetchall()
                     return results
                 except MySQLdb.MySQLError as e:
                     print(f"資料讀取錯誤: {e}")
                     return None
+
+
+
